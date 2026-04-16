cd87470fe07b38edfeff373b97641d52ff16f62b
redppgppc <44011318+redppgppc@users.noreply.github.com>
2026-04-16 15:22:36 +0900

    set Font for lbl1 in ui.py

--- ui.py
+++ ui.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
                              QLineEdit, QComboBox, QLabel)
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
+
 from PyQt5 import QtCore
 
 
@@ -16,6 +17,7 @@ class View(QWidget):
         self.te1.setReadOnly(True)
         
         self.lbl1 = QLabel('v2.3.0', self)
+        self.lbl1.setFont(QFont('Consolas', 10))
 
         self.btn1=QPushButton('Calc',self)
         self.btn2=QPushButton('Clear',self)
@@ -39,7 +41,7 @@ class View(QWidget):
         hbox_formular.addWidget(self.le2)
         
         hbox = QHBoxLayout()
-        hbox.addStretch(1)
+        # hbox.addStretch(1)
         hbox.addWidget(self.lbl1)
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
